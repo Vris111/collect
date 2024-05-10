3375ab7b60723b264317838aa9b91d37f52800c8
Vris111 <ruslansimanovic56@mail.ru>
2024-05-10 19:09:42 +0700

    da1

--- tests/CollectTest.php
+++ tests/CollectTest.php
@@ -52,64 +52,33 @@ class CollectTest extends TestCase
         $this->assertEquals(['a' => 1, 'b' => 2, 'c' => 3], $collection->toArray());
     }
 
-    public function testSearch()
+    public function testPush()
     {
-        $collection = new Collect\Collect([
-            ['name' => 'Alice', 'age' => 30],
-            ['name' => 'Bob', 'age' => 25],
-            ['name' => 'Charlie', 'age' => 30],
-        ]);
-        $this->assertEquals([
-            ['name' => 'Alice', 'age' => 30],
-            ['name' => 'Charlie', 'age' => 30],
-        ], $collection->search('age', 30)->toArray());
-    }
-
-    public function testMap()
-    {
-        $collection = new Collect\Collect([1, 2, 3]);
-        $this->assertEquals([2, 4, 6], $collection->map(fn ($x) => $x * 2)->toArray());
+        $collection = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
+        $this->assertEquals(['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4], $collection->push(4, 'd')->toArray());
     }
 
     public function testUnshift()
     {
-        $collection = new Collect\Collect([1, 2, 3]);
-        $collection->unshift(0);
-        $this->assertEquals([0, 1, 2, 3], $collection->toArray());
+        $collection = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
+        $this->assertEquals([0, 'a' => 1, 'b' => 2, 'c' => 3], $collection->unshift(0)->toArray());
     }
 
     public function testShift()
     {
-        $collection = new Collect\Collect([1, 2, 3]);
-        $collection->shift();
-        $this->assertEquals([2, 3], $collection->toArray());
+        $collection = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
+        $this->assertEquals(['b' => 2, 'c' => 3], $collection->shift()->toArray());
     }
 
     public function testPop()
     {
-        $collection = new Collect\Collect([1, 2, 3]);
-        $collection->pop();
-        $this->assertEquals([1, 2], $collection->toArray());
+        $collection = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
+        $this->assertEquals(['a' => 1, 'b' => 2], $collection->pop()->toArray());
     }
 
-    public function testFilter()
+    public function testSplice()
     {
-        $collection = new Collect\Collect([1, 2, 3, 4]);
-        $filtered = $collection->filter(fn ($x) => $x % 2 !== 0)->toArray();
-        $this->assertEquals([1, 3], $filtered);
-    }
-
-    public function testEach()
-    {
-        $collection = new Collect\Collect([1, 2, 3]);
-        $collection->each(fn ($x) => $x *= 2);
-        $this->assertEquals([1, 2, 3], $collection->toArray());
-    }
-
-    public function testPush()
-    {
-        $collection = new Collect\Collect([1, 2, 3]);
-        $collection->push(4);
-        $this->assertEquals([1, 2, 3, 4], $collection->toArray());
+        $collection = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
+        $this->assertEquals(['b' => 2, 'c' => 3], $collection->splice(['a' => 1, 'b' => 2, 'c' => 3], 1, 1)->toArray());
     }
 }
